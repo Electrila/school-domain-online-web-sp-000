8dbf976e3f097fb0a39fff1229e9bd202a1afa8e
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-22 20:23:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
   def add_student(name, grade)
     @name = name
     @grade = grade
-    @roster[@grade] = []
+    @roster[@grade] = [] if @roster[@grade] == nil
     @roster[@grade] << @name
   end
   
